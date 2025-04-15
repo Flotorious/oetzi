253b3ff7e2ad1367c5c6496839ebedd33aba00a3
Daniel Tircob <d.tircob95@gmail.com>
2025-04-15 18:24:08 +0200

    Add user dashboard / profile

--- src/Controller/Admin/UserCrudController.php
+++ src/Controller/Admin/UserCrudController.php
@@ -2,27 +2,48 @@
 
 namespace App\Controller\Admin;
 
-use App\Entity\UserOld;
+use App\Entity\User;
+use Doctrine\ORM\QueryBuilder;
+use EasyCorp\Bundle\EasyAdminBundle\Collection\FieldCollection;
+use EasyCorp\Bundle\EasyAdminBundle\Collection\FilterCollection;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
-use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
-use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
-use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Dto\EntityDto;
+use EasyCorp\Bundle\EasyAdminBundle\Dto\SearchDto;
+use Symfony\Component\Security\Core\Exception\AccessDeniedException;
 
 class UserCrudController extends AbstractCrudController
 {
     public static function getEntityFqcn(): string
     {
-        return UserOld::class;
+        return User::class;
     }
 
-    /*
-    public function configureFields(string $pageName): iterable
+    public function createIndexQueryBuilder(SearchDto $searchDto, EntityDto $entityDto, FieldCollection $fields, FilterCollection $filters): QueryBuilder
     {
-        return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
-        ];
+        $userId = $this->getUser()->getId();
+
+        return parent::createIndexQueryBuilder($searchDto, $entityDto, $fields, $filters)
+            ->andWhere('entity.id = :userId')
+            ->setParameter('userId', $userId);
     }
-    */
+
+    public function findOneEntity($entityId)
+    {
+        $entity = parent::findOneEntity($entityId);
+
+        if ($entity instanceof User && $entity->getId() !== $this->getUser()->getId()) {
+            throw new AccessDeniedException('You cannot access this user.');
+        }
+
+        return $entity;
+    }
+
+    public function configureActions(Actions $actions): Actions
+    {
+        return $actions
+            ->disable(Action::NEW, Action::DELETE, Action::BATCH_DELETE);
+    }
 }
--- src/Controller/User/UserDashboardController.php
+++ src/Controller/User/UserDashboardController.php
@@ -1,21 +1,27 @@
 <?php
 
-namespace App\Controller\Admin;
+namespace App\Controller\User;
 
+use App\Controller\Admin\UserCrudController;
 use App\Entity\User;
 use EasyCorp\Bundle\EasyAdminBundle\Attribute\AdminDashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
 use Symfony\Component\HttpFoundation\Response;
+use EasyCorp\Bundle\EasyAdminBundle\Router\AdminUrlGenerator;
 
-#[AdminDashboard(routePath: '/admin', routeName: 'admin')]
-class DashboardController extends AbstractDashboardController
+#[AdminDashboard(routePath: '/profile', routeName: 'app_user_dashboard')]
+class UserDashboardController extends AbstractDashboardController
 {
+    public function __construct(private AdminUrlGenerator $adminUrlGenerator)
+    {
+    }
+
+
+    #[Route('/dashboard', name: 'user_dashboard_index')]
     public function index(): Response
     {
-        return parent::index();
-
         // Option 1. You can make your dashboard redirect to some common page of your backend
         //
         // 1.1) If you have enabled the "pretty URLs" feature:
@@ -34,18 +40,28 @@ class DashboardController extends AbstractDashboardController
         // Option 3. You can render some custom template to display a proper dashboard with widgets, etc.
         // (tip: it's easier if your template extends from @EasyAdmin/page/content.html.twig)
         //
-        // return $this->render('some/path/my-dashboard.html.twig');
+        return $this->render('user_dashboard/index.html.twig', [
+            'user' => $this->getUser(),
+        ]);
     }
 
     public function configureDashboard(): Dashboard
     {
         return Dashboard::new()
-            ->setTitle('App');
+            ->setTitle('User Dashboard');
     }
 
     public function configureMenuItems(): iterable
     {
-        yield MenuItem::linkToDashboard('Dashboard', 'fa fa-home');
-        yield MenuItem::linkToCrud('Users', 'fas fa-list', User::class);
+        $userId = $this->getUser()->getId();
+
+        $editProfileUrl = $this->adminUrlGenerator
+            ->setController(UserCrudController::class)
+            ->setAction('edit')
+            ->setEntityId($userId)
+            ->generateUrl();
+
+        yield MenuItem::linkToUrl('My Profile', 'fa fa-user', $editProfileUrl);
+        yield MenuItem::linkToLogout('Logout', 'fa fa-sign-out');
     }
 }
