ecfcbb7e650028fc01b0924d855d2de2e82bf5b6
Daniel Tircob <d.tircob95@gmail.com>
2025-05-21 16:44:56 +0200

    Display user in enerySnapshot list

--- src/Controller/Admin/UserEnergySnapshotCrudController.php
+++ src/Controller/Admin/UserEnergySnapshotCrudController.php
@@ -4,7 +4,9 @@ namespace App\Controller\Admin;
 
 use App\Entity\UserEnergySnapshot;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\DateTimeField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\NumberField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 
@@ -15,14 +17,14 @@ class UserEnergySnapshotCrudController extends AbstractCrudController
         return UserEnergySnapshot::class;
     }
 
-    /*
     public function configureFields(string $pageName): iterable
     {
         return [
             IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
+            DateTimeField::new('timestamp'),
+            NumberField::new('consumptionKwh'),
+            NumberField::new('consumptionDelta'),
+            TextField::new('user.email', 'Owner')->onlyOnIndex(),
         ];
     }
-    */
 }
